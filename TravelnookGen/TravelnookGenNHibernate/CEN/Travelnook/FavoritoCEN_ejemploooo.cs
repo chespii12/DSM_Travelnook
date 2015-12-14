75c9e1be6f39687e3d479b32ed8dc85538b00ce9
sergiocastro95 <sergiocastrosola@hotmail.com>
2015-12-14 20:30:12 +0100

    huhuh9hi

---
+++ TravelnookGen/TravelnookGenNHibernate/CEN/Travelnook/FavoritoCEN_ejemploooo.cs
@@ -0,0 +1,28 @@
+
+using System;
+using System.Text;
+
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Criterion;
+using NHibernate.Exceptions;
+
+using TravelnookGenNHibernate.EN.Travelnook;
+using TravelnookGenNHibernate.CAD.Travelnook;
+
+namespace TravelnookGenNHibernate.CEN.Travelnook
+{
+public partial class FavoritoCEN
+{
+public void Ejemploooo (int p_oid)
+{
+        /*PROTECTED REGION ID(TravelnookGenNHibernate.CEN.Travelnook_Favorito_ejemploooo) ENABLED START*/
+
+        // Write here your custom code...
+
+        throw new NotImplementedException ("Method Ejemploooo() not yet implemented.");
+
+        /*PROTECTED REGION END*/
+}
+}
+}
