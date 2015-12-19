f6b3552c3cb4da691de98180d298e2ca28da4aa2
sergiocastro95 <sergiocastrosola@hotmail.com>
2015-12-19 20:53:22 +0100

    version con mapa en los sitios

---
+++ TravelnookGen/TravelnookMVC/Models/AssemblerUsuario.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TravelnookGenNHibernate.EN.Travelnook;
+
+namespace TravelnookMVC.Models
+{
+    public class AssemblerUsuario
+    {
+        public Usuario ConvertENToModelUI(UsuarioEN en)
+        {
+            Usuario usu = new Usuario();
+            usu.Nombre = en.Nombre;
+            usu.Apellidos = en.Apellidos;
+            usu.UserName = en.NomUsu;
+            usu.Provincia = en.Provincia;
+            usu.Localidad = en.Localidad;
+            usu.Foto = en.Foto_perfil;
+            usu.Fecha = en.FechaNacimiento;
+            usu.Email = en.Email;
+            /*sit.Nombre = en.Nombre;
+            sit.NombreUsuario = en.Usuario.NomUsu;
+            sit.Localizacion = en.Localizacion;
+            sit.Provincia = en.Provincia;
+            sit.TipoSitio = en.TipoSitio;
+            sit.Videos = en.Videos;
+            sit.Fecha = en.FechaCreacion;
+            sit.Descripcion = en.Descripcion;
+            sit.fotos = en.Fotos;
+            sit.Puntuacion = en.PuntuacionMedia;
+            IList<Comentario> comentarios = new List<Comentario>();
+            AssemblerComentario auxiliar = new AssemblerComentario();
+            Comentario auxiliar2 = new Comentario();
+            foreach (ComentarioEN com in en.Comentarios)
+            {
+                auxiliar2 = auxiliar.ConvertENToModelUI(com);
+                //sit.Comentarios.Add(new AssemblerComentario (ConvertENToModelUI (com)));
+                comentarios.Add(auxiliar2);
+                //comentarios.Add(com.Id);
+            }
+            sit.Comentarios = comentarios;
+
+            IList<TravelnookGenNHibernate.Enumerated.Travelnook.TipoActividadesEnum> actividades = new List<TravelnookGenNHibernate.Enumerated.Travelnook.TipoActividadesEnum>();
+            foreach (ActividadEN act in en.Actividades)
+            {
+                actividades.Add(act.Tipo);
+            }
+            sit.Actividades = actividades;
+            IList<string> actividadesimprimir = new List<string>();
+            foreach (ActividadEN act in en.Actividades)
+            {
+                string aux = Convert.ToString(act.Tipo);
+                actividadesimprimir.Add(aux);
+            }
+            sit.Actividadesimprimir = actividadesimprimir;*/
+            return usu;
+
+
+        }
+        public IList<Usuario> ConvertListENToModel(IList<UsuarioEN> ens)
+        {
+            IList<Usuario> sits = new List<Usuario>();
+            foreach (UsuarioEN en in ens)
+            {
+                sits.Add(ConvertENToModelUI(en));
+            }
+            return sits;
+        }
+
+    }
+}
---
+++ TravelnookGen/TravelnookMVC/Models/Usuario.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+namespace TravelnookMVC.Models
+{
+    public class Usuario
+    {
+        [ScaffoldColumn(false)]
+        public IList<Sitio> sitios { get; set; }
+
+        [ScaffoldColumn(false)]
+        public IList<Ruta> rutas { get; set; }
+        
+        //PONER EVENTO
+        //poner amigos
+        //PONER PETICIONES
+        
+
+        [Required]
+        [Display(Name = "Nombre")]
+        public string Nombre { get; set; }
+
+        [Required]
+        [Display(Name = "Apellidos")]
+        public string Apellidos { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre de usuario")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Correo electrónico")]
+        [EmailAddress(ErrorMessage = "Correo electrónico no válido")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "El número de caracteres de {0} debe ser al menos {2}.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar contraseña")]
+        [Compare("Password", ErrorMessage = "La contraseña y la contraseña de confirmación no coinciden.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Provincia")]
+        public string Provincia { get; set; }
+
+        [Required]
+        [Display(Name = "Localidad")]
+        public string Localidad { get; set; }
+
+        [Required]
+        [Display(Name = "Fecha de nacimiento")]
+        public Nullable<DateTime> Fecha { get; set; }
+
+
+        [Display(Name = "Foto de perfil")]
+        public string Foto { get; set; }
+
+
+    }
+}
