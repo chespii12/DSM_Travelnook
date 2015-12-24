292bb037ee9cf3392c16afb4209c620d06cd319c
sergiocastro95 <sergiocastrosola@hotmail.com>
2015-12-24 14:07:56 +0100

    24 12 gen

---
+++ TravelnookGen/TravelnookGenNHibernate/CAD/Travelnook/SitioCAD_FiltroDinamico.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TravelnookGenNHibernate.CEN.Travelnook;
+using NHibernate;
+
+namespace TravelnookGenNHibernate.CAD.Travelnook
+{
+    partial class SitioCAD
+    {
+        public System.Collections.Generic.IList<TravelnookGenNHibernate.EN.Travelnook.SitioEN> Filtro_Dinamico(string sql, string p_nombre, string p_provincia, int p_puntuacion)
+        {
+            System.Collections.Generic.IList<TravelnookGenNHibernate.EN.Travelnook.SitioEN> result;
+            try
+            {
+                SessionInitializeTransaction();
+
+                IQuery query = (IQuery)session.CreateQuery(sql);
+
+                if (p_nombre != null && p_nombre != "")
+                    query.SetParameter ("p_nombre", p_nombre);
+                if (p_provincia != null && p_provincia != "")
+                    query.SetParameter ("p_provincia", p_provincia);
+                if (p_puntuacion != -1)
+                    query.SetParameter("p_puntuacion", p_puntuacion);
+
+                result = query.List<TravelnookGenNHibernate.EN.Travelnook.SitioEN>();
+
+                SessionCommit();
+            }
+
+            catch (Exception ex)
+            {
+                SessionRollBack();
+                if (ex is TravelnookGenNHibernate.Exceptions.ModelException)
+                    throw ex;
+                throw new TravelnookGenNHibernate.Exceptions.DataLayerException("Error in SitioCAD_FiltroDinamico.", ex);
+            }
+
+
+            finally
+            {
+                SessionClose();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TravelnookGen/TravelnookMVC/Models/Favorito.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+namespace TravelnookMVC.Models
+{
+    public class Favorito
+    {
+
+        [ScaffoldColumn(false)]
+        public string NombreUsuario { get; set; }
+
+
+        
+
+        /*[Display(Prompt = "Vídeos del sitio", Description = "Vídeos del sitio", Name = "Vídeos ")]
+        //[Required(ErrorMessage = "Debe indicar una imagen del artículo")]
+        public IList<string> Videos { get; set; }*/
+
+
+
+    }
+}
